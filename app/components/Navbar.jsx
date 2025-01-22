e69b97d2905665b7199d9420dbc4bf9d3d3c01ed
Dre <92935512+JPAVictoria@users.noreply.github.com>
2025-01-22 16:38:04 +0800

    update-commit

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import { useState } from "react";
 
 export default function Navbar() {
@@ -11,23 +10,37 @@ export default function Navbar() {
   };
 
   const handleHardNavigation = (url) => {
-    window.location.href = url; 
+    window.location.href = url;
   };
 
   return (
     <div>
       <div className="navbar mt-10">
-        <div className="logo">STICK IT</div>
-        <div className="menu-icon" onClick={toggleMenu} style={{ cursor: "pointer" }}>
+        {/* Make the logo clickable */}
+        <div 
+          className="logo" 
+          style={{ cursor: "pointer" }} 
+          onClick={() => handleHardNavigation("/")}
+        >
+          STICK IT
+        </div>
+        <div 
+          className="menu-icon" 
+          onClick={toggleMenu} 
+          style={{ cursor: "pointer" }}
+        >
           ☰
         </div>
         <div className={`links ${menuOpen ? "active" : "mr-32"}`}>
-          {/* Replace Link components with hard navigation */}
-          <a style={{ cursor: "pointer" }} onClick={() => handleHardNavigation("/")}>Home</a>
-          <a style={{ cursor: "pointer" }} onClick={() => handleHardNavigation("/instructions")}>Instructions</a>
-          <a style={{ cursor: "pointer" }} onClick={() => handleHardNavigation("/register")}>Register</a>
+          <a style={{ cursor: "pointer" }} onClick={() => handleHardNavigation("/instructions")}>
+            Instructions
+          </a>
+          <a style={{ cursor: "pointer" }} onClick={() => handleHardNavigation("/register")}>
+            Register
+          </a>
         </div>
       </div>
     </div>
   );
 }
+    
