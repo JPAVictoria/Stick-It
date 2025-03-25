8859bc4dcef6931bb85bec3a3446a3ca1d9c390f
Dre <92935512+JPAVictoria@users.noreply.github.com>
2025-03-25 17:16:00 +0800

    update border-width

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -215,7 +215,7 @@ export default function Header({ setNotes, setPopup, filterActive }) { // Add fi
             onDragLeave={handleDragLeaveGarbage}
             onDrop={handleDropOnGarbage}
             style={{
-              borderWidth: '3px',
+              borderWidth: '2px',
               borderStyle: 'dashed',
               borderColor: isGarbageHovered ? '#D1D7E0' : 'transparent',
               borderRadius: '10px',
